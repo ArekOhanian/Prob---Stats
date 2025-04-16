ee32233f7cfc6ab9d50ebaa39ddfb01ed45e595b
ArekOhanian <ohaniana@go.stockton.edu>
2025-04-16 17:15:26 -0400

    updated the hash map code

--- Project2/Hash/TestHashing.java
+++ Project2/Hash/TestHashing.java
@@ -3,18 +3,15 @@ import java.util.LinkedList;
 
 public class TestHashing {
     public static void main(String[] args){
-        
-        YourNameSimpleHashMap hash = new YourNameSimpleHashMap(null, 0);
+        YourNameSimpleHashMap hash = new YourNameSimpleHashMap(5);
+        hash.put("Hello");
+        hash.put("world");
+        hash.put("what");
+        hash.put("why");
 
-
+        System.out.println(hash.get("Hello"));
+        System.out.println(hash.contains("Hello"));
+        System.out.println(hash.get("world"));
     }
 
-    //here is the resize method where we take the old array and copy it to a new one by removing the last indicy
-    public LinkedList<String>[] resize(LinkedList<String>[] inputHash){
-        LinkedList<String>[] outputHash = new LinkedList<String>[inputHash.length - 1]; 
-        for(int i = 0; i < inputHash.length - 1; i++){
-            outputHash[i] = inputHash[i];
-        }
-        return outputHash;
-    }
 }
--- Project2/Hash/YourNameSimpleHashMap.java
+++ Project2/Hash/YourNameSimpleHashMap.java
@@ -4,6 +4,15 @@ import java.util.LinkedList;
 public class YourNameSimpleHashMap {
     LinkedList<String>[] data;
 
+    //here is the constructor for the simple hash map
+    public YourNameSimpleHashMap(int size){
+        this.data = new LinkedList[size];
+        for(int i = 0; i < size; i++){
+            this.data[i] = new LinkedList<>();
+        }
+    }
+
+
     //Count the number of letters in the string to make it the hash
     public int dumbHash(String a){
         int pos = a.length();
@@ -11,20 +20,47 @@ public class YourNameSimpleHashMap {
     }
 
     //To check if the hash contains a certain value
-    public boolean contains(LinkedList<String> b, String specific){
-        if(b.contains(specific)){
-        return true;
-        }
-        return false;
+    public boolean contains(String specific){
+        int pos = dumbHash(specific);
+        return data[pos].contains(specific);
     }
 
-    public YourNameSimpleHashMap(LinkedList<String>[] data){
-        this.data = data;
+
+    //put function
+    public void put(String userString){
+        int pos = dumbHash(userString);
+        while(pos >= data.length){
+            this.data = resize(data);
+        }
+        if(!data[pos].contains(userString)){
+            data[pos].add(userString);
+        }
     }
+
+
+    //get function where we input a string and it searches the map and returns the position if it is not in the hash it should return null
+    public Integer get(String key){
+        int pos = dumbHash(key);
+        if(contains(key)){
+            return pos;
+        }
+        return null;
+    }
+
 
     //getters and setters
     public LinkedList<String>[] getData(){
         return data;
     }
     
+    //here is the resize method where we take the old array and copy it to a new one and add one
+    public LinkedList<String>[] resize(LinkedList<String>[] inputHash){
+        LinkedList<String>[] outputHash = new LinkedList[inputHash.length + 1]; 
+        for(int i = 0; i < inputHash.length; i++){
+            outputHash[i] = inputHash[i];
+        }
+
+        outputHash[outputHash.length - 1] = new LinkedList<>();
+        return outputHash;
+    }
 }
